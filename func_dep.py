86b8d095fc1cd2be6dfd50aa39d4f9fa131fd67a
m2i101 <varagantimanikanta@gmail.com>
2019-10-07 19:58:17 +0530

    functional dependencies to be installed for the cloud function

---
+++ func_dep.py
@@ -0,0 +1,17 @@
+# Function dependencies, for example:
+# version of the package might vary
+# package>=version
+
+sqlalchemy>=1.3
+
+pymysql>=0.9
+
+pandas>=0.24
+
+gcsfs>=0.2
+
+
+google-cloud-storage>=1.14
+
+
+
